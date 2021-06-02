f370a165765cf56f302549bab7bc0d59e6bdec99
AuroreBussalb <aurore.bussalb@gmail.com>
2021-06-02 14:36:58 +0200

    prepare python file

--- detect_alpha_peak.py
+++ detect_alpha_peak.py
@@ -14,14 +14,13 @@ def detect_alpha_peak():
 
 
 def main():
-	    # Generate a json.product to display messages on Brainlife UI
+
+	# Generate a json.product to display messages on Brainlife UI
     dict_json_product = {'brainlife': []}
 
     # Load inputs from config.json
     with open('config.json') as config_json:
         config = json.load(config_json)
-
-
 
     # Success message in product.json    
     dict_json_product['brainlife'].append({'type': 'success', 'msg': 'Alpha peak successfully detected.'})
@@ -29,6 +28,7 @@ def main():
 	# Save the dict_json_product in a json file
     with open('product.json', 'w') as outfile:
         json.dump(dict_json_product, outfile)
+
 
 if __name__ == '__main__':
     main()
