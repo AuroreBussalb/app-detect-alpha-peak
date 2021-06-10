4dafab7de417dff41aa9226d343fcc9ca1938b57
AuroreBussalb <aurore.bussalb@gmail.com>
2021-06-10 11:22:30 +0200

    plot psd on all channels and write csv

--- detect_alpha_peak.py
+++ detect_alpha_peak.py
@@ -9,56 +9,131 @@ import os
 import shutil
 
 
-def detect_alpha_peak(psd_welch, freqs):
-    
-    # Average across all channels
-    psd_welch_mean = np.mean(psd_welch, axis=0)
+def get_alpha_freqs(freqs):
 
     # Select band 7 - 14Hz #
+
     # Extract indices of alpha freqs
     indexes_alpha_freqs = [i for i, f in zip(range(0, len(freqs)), freqs) if f > 6.9  and f < 14.1]
     # Extract alpha freqs 
     alpha_freqs = np.take(freqs, indexes_alpha_freqs)
+
+    return alpha_freqs, indexes_alpha_freqs 
+
+
+def detect_alpha_peak_mean(psd_welch, alpha_freqs, indexes_alpha_freqs):
+
+
+    # Average PSD across all channels
+    psd_welch_mean = np.mean(psd_welch, axis=0)    
+
+    # Get the std of the mean
+    psd_welch_std = np.std(psd_welch, axis=0) 
+
     # Extract psd in alpha freqs
-    psd_in_alpha_freqs = np.take(psd_welch_mean, indexes_alpha_freqs)
+    psd_in_alpha_freqs_mean = np.take(psd_welch_mean, indexes_alpha_freqs)
 
-    # Find peak #
-    pic_loc = mne.preprocessing.peak_finder(psd_in_alpha_freqs)
+    # Find peak 
+    pic_loc = mne.preprocessing.peak_finder(psd_in_alpha_freqs_mean)
 
-    # Find the corresponding frequency #
+    # Find the corresponding frequency 
     index_of_the_pic = int(pic_loc[0])
-    alpha_freq_pic = alpha_freqs[index_of_the_pic]  # Apply the index of the pic in alpha_freqs, not in freqs!
+    alpha_freq_pic_mean = alpha_freqs[index_of_the_pic]  # Apply the index of the pic in alpha_freqs, not in freqs!
 
-    return alpha_freq_pic, psd_welch_mean, alpha_freqs, psd_in_alpha_freqs  
+    return alpha_freq_pic_mean, psd_welch_mean, psd_welch_std, psd_in_alpha_freqs_mean 
 
 
-def plot_psd(freqs, alpha_freq_pic, psd_welch_mean, alpha_freqs, psd_in_alpha_freqs):
+def detect_alpha_peak_per_channels(psd_welch, alpha_freqs, indexes_alpha_freqs):
+
+    # Get alpha peak frequency for all channels # 
+
+    alpha_freq_pic_per_channel = []
+    for channel in range(0, psd_welch.shape[0]): 
+        # Extract psd in alpha freqs
+        psd_in_alpha_freqs_per_channel = np.take(psd_welch[channel, :], indexes_alpha_freqs)
+        # Find peak 
+        pic_loc = mne.preprocessing.peak_finder(psd_in_alpha_freqs_per_channel)
+        pic_loc = pic_loc[0]
+        if len(pic_loc) > 1:  # if more than 1 peak is found
+            pic_loc = max(pic_loc)
+        # Find the corresponding frequency
+        index_of_the_pic = int(pic_loc)
+        alpha_freq_pic = alpha_freqs[index_of_the_pic]  # Apply the index of the pic in alpha_freqs, not in freqs!
+        alpha_freq_pic_per_channel.append(alpha_freq_pic)
+
+ 
+    return alpha_freq_pic_per_channel, psd_in_alpha_freqs_per_channel 
+
+
+def plot_psd_mean(freqs, alpha_freq_pic_mean, psd_welch_mean, psd_welch_std, alpha_freqs, psd_in_alpha_freqs_mean):
+
+    plt.figure()
  
     # Get the index of alpha peak
-    id_alpha_peak =  np.where(freqs==alpha_freq_pic)
+    id_alpha_peak =  np.where(freqs==alpha_freq_pic_mean)
+
+    # Define lim
+    plt.xlim(xmin=0, xmax=max(freqs))
 
     # Plot spectrum
-    plt.plot(freqs, psd_welch_mean)   
+    plt.plot(freqs, psd_welch_mean, zorder=1)   
 
     # Plot a red point on the alpha peak
-    plt.plot(alpha_freq_pic, psd_welch_mean[id_alpha_peak], marker='o', markersize=3, color="red")
+    plt.scatter(alpha_freq_pic_mean, psd_welch_mean[id_alpha_peak], marker='o', color="red", label='alpha peak frequency', zorder=3)
+
+    # Plot std of the mean as shaded area
+    plt.fill_between(freqs, psd_welch_mean-psd_welch_std, psd_welch_mean+psd_welch_std, alpha=0.5, label='standard deviation', zorder=2)
 
     # Shadow the frequencies in which we look for the peak
-    plt.ylim(ymin=min(psd_welch_mean))
-    plt.fill_between(alpha_freqs, psd_in_alpha_freqs, min(psd_welch_mean), alpha=0.2)
+    # plt.ylim(ymin=min(psd_welch_mean))
+    plt.axvline(x=min(alpha_freqs), zorder=4, color='r', linestyle='--', alpha=0.2, label='alpha band')
+    plt.axvline(x=max(alpha_freqs), zorder=4, color='r', linestyle='--', alpha=0.2)
 
     # Define labels
     plt.xlabel('Frequency (Hz)')
     plt.ylabel('Power Spectral Density')
+    plt.title('Mean power spectrum across all channels')
+    plt.legend()
 
     # Save fig
-    plt.savefig('psd.png')
+    plt.savefig('out_dir/psd_mean.png')
+
+
+def plot_psd_per_channels(freqs, alpha_freq_pic_per_channel, psd_in_alpha_freqs_per_channel, psd_welch, alpha_freqs):
+
+    plt.figure()
+ 
+    # Get the index of each alpha peak
+    # list_alpha_peak_id = []
+    # for freq_channels in alpha_freq_pic_per_channel:
+    #     id_alpha_peak = np.where(freqs==freq_channels)
+    #     list_alpha_peak_id.append(id_alpha_peak)
+
+    # Plot spectrum
+    # for channel, id_papf in zip(range(0, len(alpha_freq_pic_per_channel)), list_alpha_peak_id):
+    #     plt.plot(freqs, psd_welch[channel], zorder=1)  
+          # plt.scatter(alpha_freq_pic_per_channel[channel], psd_welch[channel, id_papf], marker='o', color="red", zorder=3, alpha=0.2, size=0.2)
+    for channel in range(0, len(alpha_freq_pic_per_channel)):
+        plt.plot(freqs, psd_welch[channel], zorder=1)  
+
+    # Define lim
+    plt.xlim(xmin=0, xmax=max(freqs))
+ 
+    # Plot alpha band
+    plt.axvline(x=min(alpha_freqs), zorder=4, color='r', linestyle='--', alpha=0.6, label='alpha band')
+    plt.axvline(x=max(alpha_freqs), zorder=4, color='r', linestyle='--', alpha=0.6)
+
+    # Define labels
+    plt.xlabel('Frequency (Hz)')
+    plt.ylabel('Power Spectral Density')
+    plt.title('Mean power spectrum across all channels')
+    plt.legend()
+
+    # Save fig
+    plt.savefig('out_dir/psd_channels.png')
 
 
 def main():
-
-    # Generate a json.product to display messages on Brainlife UI
-    dict_json_product = {'brainlife': []}
 
     # Load inputs from config.json
     with open('config.json') as config_json:
@@ -68,7 +143,7 @@ def main():
     path_to_input_file = config.pop('psd_welch')
     # To be able to read input raw datatype
     if "out_dir/." in path_to_input_file:
-    	path_to_input_file = path_to_input_file.replace('out_dir/.', 'out_dir/psd.csv')
+        path_to_input_file = path_to_input_file.replace('out_dir/.', 'out_dir/psd.csv')
 
     # Read the outputs of PSD app #
 
@@ -81,19 +156,38 @@ def main():
     freqs = df_psd_welch.columns.to_numpy()
     freqs = freqs.astype(np.float)  
 
-    # Detect alpha peak #
-    alpha_freq_pic, psd_welch_mean, alpha_freqs, psd_in_alpha_freqs = detect_alpha_peak(psd_welch, freqs)
+    # Get alpha freqs
+    alpha_freqs, indexes_alpha_freqs = get_alpha_freqs(freqs)
+
+    # Detect alpha peak on average channels #
+    alpha_freq_pic_mean, psd_welch_mean, psd_welch_std, psd_in_alpha_freqs_mean = detect_alpha_peak_mean(psd_welch, alpha_freqs, indexes_alpha_freqs)
+
+    # Detect alpha peak per channels #
+    alpha_freq_pic_per_channel, psd_in_alpha_freqs_per_channel = detect_alpha_peak_per_channels(psd_welch, alpha_freqs, indexes_alpha_freqs)
+
+
+    # Create a DataFrame with alpha peak values #
+
+    # Values for each channel
+    channels = [f"channel_{i}" for i in range(0, len(alpha_freq_pic_per_channel))]
+    d_all_channels = {'channels': channels, 'alpha peak frequency': alpha_freq_pic_per_channel} 
+    df_alpha_peaks = pd.DataFrame(data=d_all_channels)
+
+    # Value for mean PSD across channels
+    d_mean_channels = {'channels': "mean channels", 'alpha peak frequency': alpha_freq_pic_mean}
+    df_alpha_peaks = df_alpha_peaks.append(d_mean_channels, ignore_index=True)
+
+    # Save it into a csv
+    df_alpha_peaks.to_csv('out_dir/alpha_peak_frequency.csv', index=False)
+
 
     # Plot spectrum #
-    plot_psd(freqs, alpha_freq_pic, psd_welch_mean, alpha_freqs, psd_in_alpha_freqs)
 
-    # Success message in product.json #
-    success_message = f'Alpha peak successfully detected at {alpha_freq_pic:.2f}Hz.'   
-    dict_json_product['brainlife'].append({'type': 'success', 'msg': success_message})
+    # Mean spectrum
+    plot_psd_mean(freqs, alpha_freq_pic_mean, psd_welch_mean, psd_welch_std, alpha_freqs, psd_in_alpha_freqs_mean)
 
-    # Save the dict_json_product in a json file #
-    with open('product.json', 'w') as outfile:
-        json.dump(dict_json_product, outfile)
+    # All channels
+    plot_psd_per_channels(freqs, alpha_freq_pic_per_channel, psd_in_alpha_freqs_per_channel, psd_welch, alpha_freqs)
 
 
 if __name__ == '__main__':
