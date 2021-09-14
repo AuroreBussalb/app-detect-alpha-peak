fe68c37ae990f8f5460ba3e3978f06d85020d54a
Guiomar Niso <guiomar.niso@ctb.upm.es>
2021-09-14 21:36:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ alpha_channel_peak = []
 
 # Prepare for Figure 1 containing all the channels
 plt.figure(1)
-fig, axs = plt.subplots(math.ceil(nchannels/10),10, figsize=(10, math.ceil(nchannels/10*0.5)), facecolor='w', edgecolor='k')
+fig, axs = plt.subplots(math.ceil(nchannels/5),5, figsize=(20, math.ceil(nchannels/5*1)), facecolor='w', edgecolor='k')
 fig.subplots_adjust(hspace = .5, wspace=.2)
 axs = axs.ravel()
 
@@ -98,7 +98,7 @@ for channel in range(0, nchannels):
     #axs[channel].set_title(canales[channel])  
 
 #Save Figure 1  
-plt.savefig(os.path.join('out_dir2','psd_allchannels.png'),dpi=20)
+plt.savefig(os.path.join('out_dir2','psd_allchannels.png'),dpi=50)
 plt.close()
 
 # Average of the peak of all the channels
@@ -132,6 +132,6 @@ plt.ylabel('Power Spectral Density')
 
 plt.axvline(x=mean_alpha_peak,c='k',ls=':');
 # Save fig
-plt.savefig(os.path.join('out_dir2','psd_alpha_peak.png'),dpi=20)
+plt.savefig(os.path.join('out_dir2','psd_alpha_peak.png'))
 plt.close()
 
